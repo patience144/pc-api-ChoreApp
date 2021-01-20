df4c7f6215d57a27604192970bd4fcd8a24bd39c
Quiana <quianalee@Quianas-MacBook-Air.local>
2021-01-19 23:46:26 -0500

    config.js changes

--- config.js
+++ config.js
@@ -2,6 +2,6 @@ module.exports = {
   NODE_ENV: process.env.NODE_ENV || 'development',
   PORT: process.env.PORT || '5000',
   API_KEY: process.env.API_KEY || 'my-secret-key',
-  DATABASE_URL: process.env.DATABASE_URL || `postgres://fdxhyryhwdjalc:28b1431b9dc3142c53b05df8942fe05665c2137dddf595fb2da7ecba7ed19c82@ec2-34-202-65-210.compute-1.amazonaws.com:5432/ddfrmpdsnlutoi`,
+  DATABASE_URL: process.env.DATABASE_URL || `postgres://scvfmqgnshmewt:48799e70413ee6be373db55a8c3219ee3bab530dbd65b838ebcc6e4a2fc386e4@ec?sslmode=true`,
   TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://patience@localhost/choreapp-test'
 }
