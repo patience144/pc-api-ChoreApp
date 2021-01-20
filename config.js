63a77f1e58481358ae8416da1bb4564c5e8cc3a6
Quiana <quianalee@Quianas-MacBook-Air.local>
2021-01-20 11:30:11 -0500

    Port

--- config.js
+++ config.js
@@ -2,6 +2,6 @@ module.exports = {
   NODE_ENV: process.env.NODE_ENV || 'development',
   PORT: process.env.PORT || '5000',
   API_KEY: process.env.API_KEY || 'my-secret-key',
-  DATABASE_URL: process.env.DATABASE_URL || `postgres://scvfmqgnshmewt:48799e70413ee6be373db55a8c3219ee3bab530dbd65b838ebcc6e4a2fc386e4@ec?sslmode=true`,
+  DATABASE_URL: process.env.DATABASE_URL || `postgres://scvfmqgnshmewt:48799e70413ee6be373db55a8c3219ee3bab530dbd65b838ebcc6e4a2fc386e4@ec?sslmode=require`,
   TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://patience@localhost/choreapp-test'
 }
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const app = require('./index');
 const knex = require('knex');
-const { DATABASE_URL, TEST_DATABASE_URL, NODE_ENV } = require('./config');
+const { DATABASE_URL, TEST_DATABASE_URL, NODE_ENV, PORT } = require('./config');
 
 const knexInstance = knex({
   client: 'pg',
@@ -9,6 +9,6 @@ const knexInstance = knex({
 
 app.set('db', knexInstance);
 
-app.listen(5000, () => {
-  console.log('Server is listening on port 5000')
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`)
 });
